ba9bddaad80ced2c4d2d916faf533bd79109bccb
Vitalii Serbyn <vivitseser@gmail.com>
2022-12-17 01:08:06 +0200

    Install hardhat into project and add MilesAwayNFT.sol skeleton

---
+++ contracts/MilesAwayNft.sol
@@ -0,0 +1,9 @@
+// SPDX-Lisence-Identifier: MIT
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MilesAwayNft {
+    constructor() {}
+}
