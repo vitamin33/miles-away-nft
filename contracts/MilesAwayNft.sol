45c3354aa3fff4c80c32899b20a10b11915454df
Vitalii Serbyn <vivitseser@gmail.com>
2022-12-17 19:56:22 +0200

    Add MilesAwayNft.sol contract

--- contracts/MilesAwayNft.sol
+++ contracts/MilesAwayNft.sol
@@ -3,7 +3,38 @@
 pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MilesAwayNft {
-    constructor() {}
+contract MilesAwayNft is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 MAX_SUPPLY = 10;
+
+    constructor() ERC721("Miles Away NFT", "MSA") {
+
+    }
+
+    function safeMint(address to, string memory uri) public{
+        uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId <= MAX_SUPPLY, "Sorry, all NFTs have been minted!");
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 }
