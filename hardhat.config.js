0257083720a62e82b7300576468165b720dc310b
Vitalii Serbyn <vivitseser@gmail.com>
2022-12-19 00:14:48 +0200

    Add network config and change hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy")
+require("@nomiclabs/hardhat-ethers")
+require("@nomiclabs/hardhat-waffle")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: { }
+  },
   solidity: "0.8.17",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0 
+    }
+  }
 };
---
+++ helper-hardhat.config.js
@@ -0,0 +1,12 @@
+const networkConfig = {
+    31337: {
+        name: 'localhost',
+    },
+    80001: {
+        name: 'mumbai'
+    }
+}
+
+module.exports = {
+    networkConfig
+}
