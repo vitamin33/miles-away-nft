9b1f6386e3297a15c235c348c113cc8cae2b5875
Vitalii Serbyn <vivitseser@gmail.com>
2022-12-18 22:33:13 +0200

    Rename deploy script

--- scripts/deploy_miles_away_nft.js
+++ scripts/deploy_miles_away_nft.js
@@ -13,13 +13,13 @@ async function main() {
 
   const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const MilesAwayNft = await hre.ethers.getContractFactory("MilesAwayNft");
+  const milesAwayNft = await MilesAwayNft.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  await milesAwayNft.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${milesAwayNft.address}`
   );
 }
 
