f3f7293472293e992ad7ee8986cb88dcf786a6b4
Vitalii Serbyn <vivitseser@gmail.com>
2022-12-24 14:47:01 +0200

    Add mumbai network to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,25 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy")
 require("@nomiclabs/hardhat-ethers")
 require("@nomiclabs/hardhat-waffle")
+require('dotenv').config()
+
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: { }
+    hardhat: { },
+    mumbai: {
+      url: MUMBAI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      saveDeployments: true
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
   },
   solidity: "0.8.17",
   namedAccounts: {
